395e765001e04cc310860ccb3f8eb497f386baa5
sentsin <xu@sentsin.com>
2016-11-07 12:37:35 +0800

    3.0

--- src/layer.js
+++ src/layer.js
@@ -81,7 +81,7 @@ var layer = {
   },
   
   ready: function(callback){
-    var cssname = 'skinlayercss', ver = '1518';
+    var cssname = 'skinlayercss', ver = '117';
     isLayui ? layui.addcss('modules/layer/default/layer.css?v='+layer.v+ver, callback, cssname)
     : layer.link('skin/default/layer.css?v='+layer.v+ver, callback, cssname);
     return this;
