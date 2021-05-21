2636978c62f20526c73a8ad93940e118ba90a39b
贤心 <3277200+sentsin@users.noreply.github.com>
2021-05-22 00:11:09 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -19,13 +19,21 @@ var task = {
     }))
     .pipe(gulp.dest('./dist'));
     
-    return gulp.src('./src/layer.js').pipe(uglify())
+    return gulp.src('./src/layer.js').pipe(uglify({
+      output: {
+        ascii_only: true //escape Unicode characters in strings and regexps
+      }
+    }))
      .pipe(header('/*! <%= pkg.realname %>-v<%= pkg.version %> <%= pkg.description %> <%= pkg.license %> License */\n ;', {pkg: pkg}))
     .pipe(gulp.dest('./dist'));
     
   }
   ,mobile: function() {
-    return gulp.src('./src/mobile/layer.js').pipe(uglify())
+    return gulp.src('./src/mobile/layer.js').pipe(uglify({
+      output: {
+        ascii_only: true //escape Unicode characters in strings and regexps
+      }
+    }))
      .pipe(header('/*! <%= pkg.realname %> mobile-v<%= pkg.mobile %> <%= pkg.description %> <%= pkg.license %> License */\n ;', {pkg: pkg}))
     .pipe(gulp.dest('./dist/mobile'));
   }
@@ -43,21 +51,24 @@ gulp.task('layer', task.minjs); //压缩PC版本
 gulp.task('mobile', task.mincss); //压缩Mobile文件
 gulp.task('other', task.other); //移动一些配件
 
-//打包发行版
+//发行版本目录
 var releaseDir = './release/zip/layer-v' + pkg.version;
+var release = releaseDir + '/layer';
+
+//打包发行版
 gulp.task('clearZip', function(cb){ //清理
-  return del(['./release/zip/*'], cb);
+  return del([releaseDir], cb);
 });
 gulp.task('r', ['clearZip'], function(){
   gulp.src('./release/doc/**/*')
-  .pipe(gulp.dest(releaseDir))
+  .pipe(gulp.dest(releaseDir));
   
   return gulp.src([
     './dist/**/*'
     ,'!./dist/**/moon'
     ,'!./dist/**/moon/*'
   ])
-  .pipe(gulp.dest(releaseDir + '/layer'))
+  .pipe(gulp.dest(release));
 });
 
 //全部
